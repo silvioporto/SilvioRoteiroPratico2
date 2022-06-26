75366e3281e7aaabb27ffbcec86bd1acf4c724af
Silvio Porto <103378951+silvioporto@users.noreply.github.com>
2022-06-26 06:04:45 +0000

    Commit 15

--- Price.java
+++ Price.java
@@ -3,11 +3,6 @@ public abstract class Price {
   public abstract double getCharge(int daysRented);
 
   public int getFrequentRenterPoints(int daysRented) {
-     if (this.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
-         return 2;
-     } else {
-         return 1;
-     }
-  }
-
+       return 1;
+   }
 }
